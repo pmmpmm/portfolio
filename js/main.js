b4bc92aee819709e850a0b9a7ca4f8f886e41edf
kimkyounghwan <kh_kim@brighant.com>
2022-11-05 00:44:45 +0900

    header transparent

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-// test
+'use strict';
+
+// 헤더 스크롤 이벤트
+const header = document.querySelector('.header');
+let scrollY = window.scrollY;
+let heightH = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+  if (scrollY > heightH) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+});
